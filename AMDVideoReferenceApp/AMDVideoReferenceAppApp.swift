3d56ddaea1320d02e9bd0aa367216c1a6a436c25
Erik Heath A Thomas <erikheath.thomas.contractor@weathergroup.com>
2024-08-26 15:26:35 -0400

    Initial Commit

---
+++ AMDVideoReferenceApp/AMDVideoReferenceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMDVideoReferenceAppApp.swift
+//  AMDVideoReferenceApp
+//
+//  Created by Erik Heath Thomas on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AMDVideoReferenceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
